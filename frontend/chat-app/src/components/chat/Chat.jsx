d7c931126900662c83769f00ff6b500aa691ff06
Pramuditha <pramudithakavinda01@gmail.com>
2024-08-26 21:03:02 +0530

    Update List

--- frontend/chat-app/src/components/chat/Chat.jsx
+++ frontend/chat-app/src/components/chat/Chat.jsx
@@ -28,9 +28,81 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
+
       <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <img src="./avatar.png" alt="" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto at 
+              molestias dignissimos non? Architecto voluptas et cumque ut libero tempore, autem 
+              dolorum delectus veniam ea, magni molestias quibusdam facilis?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto at 
+              molestias dignissimos non? Architecto voluptas et cumque ut libero tempore, autem 
+              dolorum delectus veniam ea, magni molestias quibusdam facilis?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto at 
+              molestias dignissimos non? Architecto voluptas et cumque ut libero tempore, autem 
+              dolorum delectus veniam ea, magni molestias quibusdam facilis?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto at 
+              molestias dignissimos non? Architecto voluptas et cumque ut libero tempore, autem 
+              dolorum delectus veniam ea, magni molestias quibusdam facilis?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto at 
+              molestias dignissimos non? Architecto voluptas et cumque ut libero tempore, autem 
+              dolorum delectus veniam ea, magni molestias quibusdam facilis?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto at 
+              molestias dignissimos non? Architecto voluptas et cumque ut libero tempore, autem 
+              dolorum delectus veniam ea, magni molestias quibusdam facilis?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
 
       </div>
+
+      
 
       <div className="bottom">
         <div className="icons">
@@ -47,7 +119,10 @@ const Chat = () => {
           <img src="./emoji.png" 
           alt="" 
           onClick={() => setOpen((prev)=>!prev)}/>
-          <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+          </div>
+          
         </div>
         <button className="sendButton">Send</button>
       </div>
--- frontend/chat-app/src/components/detail/Detail.jsx
+++ frontend/chat-app/src/components/detail/Detail.jsx
@@ -1,7 +1,49 @@
 import "./detail.css"
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <img src="avetar.png" alt="" />
+              <span>photo_2024.png</span>
+              <img src="./download.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+
+    </div>
   )
 }
 

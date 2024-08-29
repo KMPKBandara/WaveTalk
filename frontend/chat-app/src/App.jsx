661de7e0cc77b5e458b28d5899c7dde5e58ef29a
Pramuditha <pramudithakavinda01@gmail.com>
2024-08-29 20:50:43 +0530

    Update sign in

--- frontend/chat-app/src/App.jsx
+++ frontend/chat-app/src/App.jsx
@@ -1,16 +1,36 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/list";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./components/lib/firebase";
+import { useUserStore } from "./components/lib/userStore";
 
 
 const App = () => {
-  const user = false;
+
+  const {currentUser,isLoading,fetchUserInfo} = useUserStore()
+
+  useEffect(()=> {
+    const unSub = onAuthStateChanged(auth,(user) => {
+      fetchUserInfo(user?.uid);
+    
+  });
+
+  return () => {
+    unSub();
+  };
+}, [fetchUserInfo]);
+
+console.log(currentUser);
+
+if (isLoading) return <div className="loading">Loading...</div>
 
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
           <>
           <List />
           <Chat />
